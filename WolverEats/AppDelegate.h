15ddbfe6608c21a4ffab8f75bddaf0c76382b4c9
James Lennon <jameslennon321@gmail.com>
2015-02-03 20:51:06 -0800

    Initial commit

---
+++ WolverEats/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WolverEats
+//
+//  Created by James Lennon on 2/3/15.
+//  Copyright (c) 2015 James Lennon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WolverEats/FirstViewController.m
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.m
+//  WolverEats
+//
+//  Created by James Lennon on 2/3/15.
+//  Copyright (c) 2015 James Lennon. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
